c9d1bf7c3a34965661d798038fbae6f395566e21
NurlanRahimli <nurlanshr@code.edu.az>
2023-01-13 12:57:44 +0400

    Update Account and Create Account page

--- assets/js/main.js
+++ assets/js/main.js
@@ -698,6 +698,24 @@ $(document).ready(function () {
 
 })
 
+//Show password
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+    // toggle the type attribute
+    const type = password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+
+    // toggle the icon
+    this.classList.toggle("bi-eye");
+});
+
+// prevent form submit
+const form = document.querySelector("form");
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
 
 
 //Shop-Accordion
@@ -846,4 +864,6 @@ tabs.onclick = e => {
         element.classList.add("active");
     }
 }
+
+
 
